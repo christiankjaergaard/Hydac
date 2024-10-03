b7c225b0c3e2a026c6b7f85f99ff35045f34a737
Albert <Alberttrabergmoth@gmail.com>
2024-10-03 10:33:46 +0200

    test

---
+++ Persistenstest/Persistens/Persistens/DataHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Persistens
+{
+    public class DataHandler
+    {
+        private string _dataFileName;
+
+        public string DataFileName
+        {
+            get { return _dataFileName; }
+        }
+
+        public DataHandler(string dataFileName)
+        {
+            _dataFileName = dataFileName;
+        }
+
+        public void SavePerson(Person person) 
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(DataFileName);
+                sw.WriteLine(person.MakeTitle());
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+    }
+}
---
+++ Persistenstest/Persistens/Persistens/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistens
+{
+    public class Person
+    {
+        private string _name;
+        private DateTime _birthDate;
+        private double _height;
+        private bool _isMarried;
+        private int _noOfChildren;
+        public string Name 
+        { 
+            get {return _name;} 
+            set {_name = value;} 
+        }
+        public DateTime BirthDate 
+        { 
+            get { return _birthDate; } 
+            set { _birthDate = value; } 
+        }
+        public double Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+        public bool IsMarried
+        {
+            get { return _isMarried; }
+            set { _isMarried = value; }
+        }
+        public int NoOfChildren
+        {
+            get { return _noOfChildren; }
+            set { _noOfChildren = value; }
+        }
+
+        public Person(string name, DateTime birthDate, double height, bool isMarried, int noOfChildren)
+        {
+            _name = name;
+            _birthDate = birthDate;
+            _height = height;
+            _isMarried = isMarried;
+            _noOfChildren = noOfChildren;
+        }   
+
+        public string MakeTitle()
+        { 
+            return $"{Name};{BirthDate.ToString()};{Height};{IsMarried};{NoOfChildren}";
+        }
+    }
+}
